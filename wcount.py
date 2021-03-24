4e4efbb8b59935138fbc472943795ea58a051c24
Alexander Gaenko <galexv@umich.edu>
2021-03-24 13:23:14 -0400

    Initial commit: word counting program

---
+++ wcount.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+import sys
+
+def count_from_file(filename):
+    nletters = 0
+    nwords = 0
+    npars = 0
+    with open(filename, "rt") as fd:
+        for par in fd:
+            nletters += len(par)
+            par = par.rstrip()
+            npars += 1
+            for word in par.split():
+                nwords += 1
+    return nletters, nwords, npars
+
+if __name__=='__main__':
+    nletters, nwords, npars = count_from_file(sys.argv[1])
+    print(f"letters:{nletters} words:{nwords} paragraphs:{npars}")
